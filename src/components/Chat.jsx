d399cf1539feaaff67eaa925107b6bb7935918d1
mahbub-repliq <mahbubul.alam@repliq.dev>
2023-11-04 19:17:47 +0600

    basic UI completed

---
+++ src/components/Chat.jsx
@@ -0,0 +1,88 @@
+import {
+  BsCameraVideo,
+  BsPersonCircle,
+  BsSearch,
+  BsTelephone,
+} from "react-icons/bs";
+import { AiOutlinePlus, AiOutlineSend } from "react-icons/ai";
+import { FaMicrophone } from "react-icons/fa";
+import { BiSmile } from "react-icons/bi";
+
+import chatBg from "/assets/images/whats-app-bg.png";
+import { useState } from "react";
+
+function Chat() {
+  const [receive, setReceive] = useState(true);
+  return (
+    <div className="w-full">
+      {/* chat head */}
+      <div className="flex justify-between items-center px-5 py-3 bg-[#202C33]">
+        <div className="w-1/3 flex gap-3 rounded-full">
+          <BsPersonCircle size={40} />
+          <div className="">
+            <h2 className="">Title</h2>
+            <p className="text-xs">last seen</p>
+          </div>
+        </div>
+        <div className="w-2/3 flex justify-end items-center gap-5">
+          {" "}
+          <div className="outline outline-1 outline-gray-500 p-2 rounded flex justify-center items-center gap-3 divide-x divide-gray-500">
+            <BsCameraVideo className="cursor-pointer" size={20} />
+            <div className="pl-3">
+              <BsTelephone className="cursor-pointer" size={20} />
+            </div>
+          </div>
+          <BsSearch className="" size={20} />
+        </div>
+      </div>
+
+      {/* chat body */}
+      <div
+        className="h-[80vh] space-y-3"
+        style={{ backgroundImage: `url(${chatBg})`, backgroundSize: "cover" }}
+      >
+        <div className="px-10">
+          <div className="flex justify-end">
+            <div className=" bg-[#005C4B] px-2 py-1 rounded">
+              <p>hello dssjdfggjdhkg adfgjdkfghh</p>
+              <p className="flex justify-end text-xs">7:35 AM</p>
+            </div>
+          </div>
+        </div>
+        <div className="px-10">
+          <div className={`flex ${receive && "justify-start"}`}>
+            <div className=" bg-[#202C33] px-2 py-1 rounded">
+              <p>hello dssjdfggjdhkg adfgjdkfghh</p>
+              <p className="flex justify-end text-xs">7:35 AM</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* chat footer */}
+      <div className=" px-5 py-3 bg-[#202C33]">
+        <div className="flex justify-between items-center gap-3">
+          <AiOutlinePlus className="cursor-pointer hover:rotate-9" size={20} />
+          <div className="w-full relative">
+            <BiSmile
+              className="absolute top-2.5 left-2.5 cursor-pointer"
+              size={20}
+            />
+            <input
+              className="w-full bg-[#2A3942] rounded focus:outline-none py-2 pl-10"
+              type="text"
+              name=""
+              id=""
+            />
+          </div>
+          {receive ? (
+            <AiOutlineSend className="cursor-pointer" size={20} />
+          ) : (
+            <FaMicrophone className="cursor-pointer" size={20} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,18 @@
+import Chat from "../../components/Chat";
+import Sidebar from "../../components/Sidebar";
+
 function Home() {
-  return <div className=" text-5xl text-green-600">Home</div>;
+  return (
+    <div className="flex divide-x divide-gray-600">
+      <div className="w-1/3">
+        <Sidebar />
+      </div>
+      <div className="w-2/3">
+        {" "}
+        <Chat />{" "}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
