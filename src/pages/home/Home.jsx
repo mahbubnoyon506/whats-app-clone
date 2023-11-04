8ea08d5c63b4ff62d57c3b8c407bc3516d731e62
mahbub-repliq <mahbubul.alam@repliq.dev>
2023-11-04 14:37:04 +0600

    project setup completed

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,5 @@
+function Home() {
+  return <div className=" text-5xl text-green-600">Home</div>;
+}
+
+export default Home;
---
+++ src/routes/routes.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../layout/Layout";
+import Home from "../pages/home/Home";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+export default routes;
